fd9231e8cc4b8be11796c76cb7db7c7c3a8286ff
Jianbai Ye <jianbaiye@outlook.com>
2023-08-13 21:57:09 +0800

    feat: add app rows

--- pages/applications.vue
+++ pages/applications.vue
@@ -1,24 +1,83 @@
 <template>
 	<view class="page">
-		<view class="nodata">
+		<!-- <view class="nodata">
 			<image src="https://miniprogram-public-hk.oss-cn-hongkong.aliyuncs.com/images/app_star.svg" style="max-width: 30%;"></image>
 		</view>
 		<view class="tab_box">
 			<view>Coming soon ...</view>
+		</view> -->
+		
+		<view class="app-list">
+			<view class="item">
+				<view @click="navTo(`/pages/applications/detail/detail?id=ICML`)">
+					<image src="https://miniprogram-public-hk.oss-cn-hongkong.aliyuncs.com/images/app_star.png" style="width: 100rpx;" mode="widthFix"></image>
+				</view>
+				<view class="title bolder" @click="navTo(`/pages/applications/detail/detail?id=ICML`)">
+					ICML
+				</view>
+			</view>
+			
+			<view class="item">
+				<view @click="navTo(`/pages/applications/detail/detail?id=研报总结`)">
+					<image src="https://miniprogram-public-hk.oss-cn-hongkong.aliyuncs.com/images/app_star.png" style="width: 100rpx;" mode="widthFix"></image>
+				</view>
+				<view class="title bolder" @click="navTo(`/pages/applications/detail/detail?id=ICML`)">
+					研报总结
+				</view>
+			</view>
+			
+			<view class="item">
+				<view>
+					<image src="https://miniprogram-public-hk.oss-cn-hongkong.aliyuncs.com/images/app_noready.png" style="width: 80rpx;" mode="widthFix"></image>
+				</view>
+				<view class="title bolder">
+					Coming soon ...
+				</view>
+			</view>
+			
 		</view>
-
 		<!-- <xWxmlToCanvas ref="xWxmlToCanvas" :hide="false" :width="350" :height="500" :xStyle="style" :xWxml="wxml">
 		</xWxmlToCanvas> -->
 	</view>
 </template>
 
-
 <script>
+	import {
+		getUser,
+		setUser,
+		removeUser,
+	} from '@/utils/auth'
+	import {
+		searchPaper,
+		searchKeyWords,
+		searchMyKeyWords,
+		addRead,
+		cancelRead,
+		searchMyRead
+	} from '@/api/home/index.js'
+	import config from '@/config'
+	import fastTab from '@/components/fast-tab/fast-tab';
+	// import xWxmlToCanvas from 'x-wxml-to-canvas/x-wxml-to-canvas'
+	export default {
+		onLoad() {
+			var that = this;
+
+			// that.getSubscribe();
+
+		},
+		methods: {
+			navTo(url) {
+				console.log("Nav to", url);
+				this.$tab.navigateTo(url)
+			}
+		}
+	}
 </script>
+
 
 <style lang="scss">
 	.page {
-		padding-top: 10%;
+		padding-top: 40%;
 		// background-image: url("https://miniprogram-public-hk.oss-cn-hongkong.aliyuncs.com/images/app_background.svg");
 		// background-size:     cover;                      /* <------ */
 		// background-repeat:   no-repeat;
@@ -31,4 +90,68 @@
 		font-size: 32rpx;
 		font-weight: bold;
 	}
+	
+	.app-list {
+		// padding-top: ;
+		display: flex;
+		// flex-direction: row;
+		// align-items: flex-end;
+		justify-content: center;
+		// width: 100%;
+		// column-count: 3;
+		// column-gap: 5rpx;
+		// padding-top: 150rpx;
+	
+		.item {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			  /* align-self: center;    <---- REMOVE */
+			align-items: center;
+			
+			width: calc(60% - 20rpx);
+			break-inside: avoid;
+			margin: 20rpx;
+			// margin-bottom: 30rpx;
+			// padding-bottom: 10rpx;
+			// box-sizing: border-box;
+			// // background-color: #fff;
+			// border-radius: 6rpx;
+	
+			// image {
+			// 	width: 100%;
+			// 	display: block;
+			// 	border-radius: 6rpx;
+			// }
+	
+			.title {
+				width: 100%;
+				// max-height: 86rpx;
+				text-align: center;
+				// margin-top: 4px;
+				
+				// padding: 0rpx 0rpx;
+				color: #333;
+				font-size: 32rpx;
+				word-break: normal;
+				// overflow: hidden;
+				// text-overflow: ellipsis;
+				// -webkit-line-clamp: 2;
+				// display: -webkit-box;
+				// -webkit-box-orient: vertical
+			}
+			
+			// .thumb {
+			// 	width: 100%;
+				
+			// 	// height: 176rpx;
+			// 	image {
+			// 		width: 100rpx;
+			// 		height: 100rpx;
+			// 		object-fit: cover; 
+			// 	}
+			// }
+		}
+	}
+	
 </style>
